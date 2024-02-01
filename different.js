243f2c3c741081e230228d178c33f542fc7490ba
developerswadheen <swadheenroy99@gmail.com>
2024-02-01 20:55:30 +0600

    first commit

---
+++ different.js
@@ -0,0 +1,10 @@
+let total = 0;
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+        total = total + i;
+        console.log('total', total)
+    }
+}
+
+console.log('all total', total)
